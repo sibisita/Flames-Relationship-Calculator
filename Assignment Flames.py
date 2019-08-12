a08de077b097f2ec7be4dac861c3f0ddafa3c01c
Sibi <sitasibi@gmail.com>
2019-08-13 01:23:54 +0530

    Main program file

---
+++ Assignment Flames.py
@@ -0,0 +1,24 @@
+x=input("Enter first name : ").upper()
+y=input ("Enter second name : ").upper()
+u,v=list(x),list(y)
+for i in x.upper():
+	if i in v:
+		u.remove(i)
+		v.remove(i)
+flames = ['Friends','Love','Affections','Marry','Enemy','Sister']
+t = len(u)+len(v)
+
+if (t ==0):
+	print ('All letters got cancelled')
+
+
+else:
+	while len(flames)>1:
+		lenflames=len(flames)
+		pointer = t % lenflames
+		if pointer == 0:
+			del(flames[-1])
+		else:
+			del(flames[pointer-1])
+
+	print('The Flames match of {} and {} is {}'.format(x,y,flames[0]))
